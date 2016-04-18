44394fa848f1f28ed8383dcb6e3e5d725c7a38a6
Alex Rea <git@area.io>
2016-04-18 10:15:46 +0100

    Allow eth_getBalance

--- web3-ddp-provider-server.js
+++ web3-ddp-provider-server.js
@@ -1,6 +1,6 @@
 Meteor.methods({
 	'web3DdpProviderExec': function(call) {
-		if (['eth_call', 'eth_sendRawTransaction', 'eth_getTransactionCount', 'eth_gasPrice', 'eth_getTransactionByHash','eth_estimateGas'].indexOf(JSON.parse(call).method) === -1) {
+		if (['eth_call', 'eth_sendRawTransaction', 'eth_getTransactionCount', 'eth_gasPrice', 'eth_getTransactionByHash','eth_estimateGas','eth_getBalance'].indexOf(JSON.parse(call).method) === -1) {
 			return new Error("This provider doesn't support that method")
 		}
 
