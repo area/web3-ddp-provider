8d4340380936c286bdb6f7df6b57b853915d4694
thiagodelgado111 <thiagodelgado111@gmail.com>
2016-05-21 01:28:36 -0300

    whitelist filter method for server

--- web3-ddp-provider-server.js
+++ web3-ddp-provider-server.js
@@ -1,6 +1,6 @@
 Meteor.methods({
 	'web3DdpProviderExec': function(call) {
-		if (['eth_call', 'eth_sendRawTransaction', 'eth_getTransactionCount', 'eth_gasPrice', 'eth_getTransactionByHash','eth_estimateGas','eth_getBalance'].indexOf(JSON.parse(call).method) === -1) {
+		if (['eth_call', 'eth_sendRawTransaction', 'eth_newBlockFilter', 'eth_newFilter', 'eth_uninstallFilter', 'eth_getTransactionCount', 'eth_gasPrice', 'eth_getTransactionByHash','eth_estimateGas','eth_getBalance'].indexOf(JSON.parse(call).method) === -1) {
 			return new Error("This provider doesn't support that method")
 		}
 		let gethAddress = '127.0.0.1';
