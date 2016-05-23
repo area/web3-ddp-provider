42c31ba9b6d450bf7c6c72d12c3c6825de0a7beb
thiagodelgado111 <thiagodelgado111@gmail.com>
2016-05-23 16:27:46 -0300

    whitelist eth_getFilterChanges and eth_getFilterLogs

--- web3-ddp-provider-client.js
+++ web3-ddp-provider-client.js
@@ -3,19 +3,34 @@ DdpProvider = function() {};
 
 DdpProvider.prototype.sendAsync = function(payload, callback) {
 
-	//We only whitelist some methods
-	if (['eth_call', 'eth_sendRawTransaction', 'eth_newBlockFilter', 'eth_newFilter', 'eth_uninstallFilter', 'eth_getTransactionCount', 'eth_gasPrice','eth_getTransactionByHash','eth_estimateGas', 'eth_getBalance'].indexOf(payload.method)===-1){
-		return callback(new Error("This provider doesn't support that method"))
-	}
+    //We only whitelist some methods
+    if ([
+            'eth_call',
+            'eth_sendRawTransaction',
+            'eth_newPendingTransactionFilter',
+            'eth_newBlockFilter',
+            'eth_newFilter',
+            'eth_uninstallFilter',
+            'eth_getFilterChanges',
+            'eth_getFilterLogs',
+            'eth_getLogs',
+            'eth_getTransactionCount',
+            'eth_gasPrice',
+            'eth_getTransactionByHash',
+            'eth_estimateGas',
+            'eth_getBalance'
+        ].indexOf(payload.method) === -1) {
+        return callback(new Error("This provider doesn't support that method"))
+    }
 
 
-	Meteor.call('web3DdpProviderExec', JSON.stringify(payload), function(err,res){
-		var error, result;
-		try {
-			result = JSON.parse(res.content);
-		}catch(e){
-			error = new Error();
-		}
-		callback(error, result);
-	});
+    Meteor.call('web3DdpProviderExec', JSON.stringify(payload), function(err, res) {
+        var error, result;
+        try {
+            result = JSON.parse(res.content);
+        } catch (e) {
+            error = new Error();
+        }
+        callback(error, result);
+    });
 }
--- web3-ddp-provider-server.js
+++ web3-ddp-provider-server.js
@@ -1,18 +1,34 @@
 Meteor.methods({
-	'web3DdpProviderExec': function(call) {
-		if (['eth_call', 'eth_sendRawTransaction', 'eth_newBlockFilter', 'eth_newFilter', 'eth_uninstallFilter', 'eth_getTransactionCount', 'eth_gasPrice', 'eth_getTransactionByHash','eth_estimateGas','eth_getBalance'].indexOf(JSON.parse(call).method) === -1) {
-			return new Error("This provider doesn't support that method")
-		}
-		let gethAddress = '127.0.0.1';
-		if (process.env.GETH_ADDRESS){
-			gethAddress = process.env.GETH_ADDRESS;
-		}
-		let gethPort = '8545';
-		if (process.env.GETH_PORT){
-			gethPort = process.env.GETH_PORT;
-		}
-		return Meteor.http.call("POST", "http://" + gethAddress + ":" + gethPort, {
-			content: call
-		});
-	}
+    'web3DdpProviderExec': function(call) {
+        if ([
+            'eth_call',
+            'eth_sendRawTransaction',
+            'eth_newPendingTransactionFilter',
+            'eth_newBlockFilter',
+            'eth_newFilter',
+            'eth_uninstallFilter',
+            'eth_getFilterChanges',
+            'eth_getFilterLogs',
+            'eth_getLogs',
+            'eth_getTransactionCount',
+            'eth_gasPrice',
+            'eth_getTransactionByHash',
+            'eth_estimateGas',
+            'eth_getBalance'
+          ].indexOf(JSON.parse(call).method) === -1) {
+            return new Error("This provider doesn't support that method")
+        }
+
+        let gethAddress = '127.0.0.1';
+        if (process.env.GETH_ADDRESS) {
+            gethAddress = process.env.GETH_ADDRESS;
+        }
+        let gethPort = '8545';
+        if (process.env.GETH_PORT) {
+            gethPort = process.env.GETH_PORT;
+        }
+        return Meteor.http.call("POST", "http://" + gethAddress + ":" + gethPort, {
+            content: call
+        });
+    }
 });
